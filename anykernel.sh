f6de29b0b64b621a7b4ff5d6832aac9e043e2165
SenseiiX <princekarlagas@gmail.com>
2025-04-07 19:02:54 +0800

    anyk3: major debloat

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=FusionX by SenX(@WazzupSensei911)--Telegram
+kernel.string=FusionX by SenX(@WazzupSensei911) --Telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
